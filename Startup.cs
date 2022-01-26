94185d70287cc877712f96b404fc5d8ba1e266b8
Henrique Eduardo Souza <hsouza.eduardo@gmail.com>
2022-01-25 21:43:44 -0300

    Segundo Commit

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace RepoAz400
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World HSOUZA!");
                 });
             });
         }
